c93aff234bd37ad39cdd7bbaa8f5f662cc163636
niux3 <renaudbourdeau@gmail.com>
2024-07-06 12:41:41 +0200

    feat: navigation continue

--- frontend/js/controllers/Navigation.js
+++ frontend/js/controllers/Navigation.js
@@ -8,6 +8,7 @@ export default class Navigation{
             this._$main.insertAdjacentHTML('beforeend', '<div class="overlay">&nbsp;</div>')
         }
         this._$overlay = document.querySelector('.overlay')
+        this._$accordions = document.querySelectorAll('nav .accordion')
     }
 
     openNav(){
@@ -25,6 +26,19 @@ export default class Navigation{
     onOverlay(){
         this._$overlay.addEventListener('pointerdown', e =>{
             this._$main.classList.remove('onNav')
+        })
+    }
+
+    onAccordion(){
+        this._$accordions.forEach($el =>{
+            $el.querySelector('button').addEventListener('pointerdown', e =>{
+                if($el.classList.contains('show')){
+                    $el.classList.remove('show')
+                }else{
+                    this._$accordions.forEach($el => $el.classList.remove('show'))
+                    $el.classList.add('show')
+                }
+            })
         })
     }
 }
